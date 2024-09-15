ea683278cefbe44081d1a713d0ccc1f84ff496ec
isaac <isaac.brigido@gmail.com>
2024-09-15 11:05:23 -0300

    feat: cria status do evento

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  enum status: { registrations_open: 'registrations_open',
+        event_in_progress: 'event_in_progress', event_closed: 'event_closed',
+        cancelled: 'cancelled'}
   belongs_to :user
 
   validates :name, :local, :period_start, presence: true
