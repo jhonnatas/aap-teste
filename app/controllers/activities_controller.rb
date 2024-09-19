346faa7285766ab85b4ed835453976fecb398732
isaac <isaac.brigido@gmail.com>
2024-09-19 11:31:29 -0300

    feat: crud das atividades

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,21 +1,59 @@
 class ActivitiesController < ApplicationController
   layout "evento_show", only: [:index, :show ]
 
+  PERMITED_PARAMS = [
+    :name, :local, :period_start, :period_end, :title, :speaker,
+    :certificate_hours, :subscriptions_open ].freeze
+
   before_action :authenticate_user!, except: %i[index show]
   before_action :load_event
+  before_action :load_activity, only: %i[ edit show update destroy]
 
 
   def index
     @activities = @event.activities.order(period_start: :desc)
   end
 
-  def show
-    @activity = Activity.find(params[:id])
+  def show;end
+  def edit;end
+
+  def new
+    @activity = @event.activities.build
+  end
+
+  def create
+    @activity = @event.activities.build activity_params
+    if @activity.save
+      redirect_to event_activities_path, notice: "Atividade cadastrada com sucesso!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @activity.update activity_params
+      redirect_to event_activities_path, notice: "Atividade atualizada com sucesso!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @activity.destroy
+    redirect_to event_activities_path, notice: "Atividade Excluida com sucesso!"
   end
 
   private
 
   def load_event
     @event = Event.find(params[:event_id])
+  end
+
+  def load_activity
+    @activity = @event.activities.find(params[:id])
+  end
+
+  def activity_params
+    params.require(:activity).permit(*PERMITED_PARAMS)
   end
 end
