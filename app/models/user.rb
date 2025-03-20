1a3f89a564f0156f441f8893ee1156a6714b8471
isaacbrds <isaac.brigido@gmail.com>
2025-03-20 18:31:52 -0300

    fix: adjusting relantionship between user and events and action text

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :events, through: :registrations
+  has_many :owned_events, class_name: "Event", foreign_key: "user_id", dependent: :destroy
   has_many :registrations
+  has_many :registered_events, through: :registrations, source: :event
   has_many :activity_registrations
   has_many :activities, through: :activity_registrations
 
